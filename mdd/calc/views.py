c75698727b5c2677c9a0c8f0ff82b8cbce0bea9c
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-19 21:10:31 +0530

    Added bmi calculating function

--- mdd/calc/views.py
+++ mdd/calc/views.py
@@ -10,3 +10,11 @@ def add(request):
     val2 = int(request.POST["num2"])
     res = val1+val2
     return render(request , 'result.html' , {'result' : res})
+
+def bmi(request):
+    feet = int(request.POST["f1"])
+    inches = int(request.POST["i1"])
+    weight = int(request.POST["w"])
+    intoInch = (feet*12) + inches
+    output = (weight * 703)/ intoInch**2
+    return render(request, 'result.html', {'result' : output})    
