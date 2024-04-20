c47c3ab3af66da626586d50377cabe9833618cb5
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-20 21:32:24 +0530

    use the destination object and render it with passing the object to the html page

--- mdd/travello/views.py
+++ mdd/travello/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.description = 'The city that never sleeps'
+    dest1.days = '7 days'
+    dest1.rating = '7 star'
+    return render(request, 'index.html' , {'dest1' : dest1})
 
 
