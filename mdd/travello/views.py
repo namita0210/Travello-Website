dd05e4ffe54d462e974c7654cd8c17c5c2b239be
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-21 22:02:36 +0530

    create 3 objects and a list containing them to render on the UI

--- mdd/travello/views.py
+++ mdd/travello/views.py
@@ -4,11 +4,27 @@ from .models import *
 
 # Create your views here.
 def index(request):
+
     dest1 = Destination()
     dest1.name = 'Mumbai'
     dest1.description = 'The city that never sleeps'
     dest1.days = '7 days'
-    dest1.rating = '7 star'
-    return render(request, 'index.html' , {'dest1' : dest1})
+    dest1.rating = '5 star'
+
+    dest2 = Destination()
+    dest2.name = 'Hyderabad'
+    dest2.description = 'Biryani First'
+    dest2.days = '4 days'
+    dest2.rating = '3 star'
+
+    dest3 = Destination()
+    dest3.name = 'Bangalore'
+    dest3.description = 'Silicon Valley of India'
+    dest3.days = '4 days'
+    dest3.rating = '7 star'
+
+    dests = [dest1,dest2,dest3]
+
+    return render(request, 'index.html' , {'dest1' : dests})
 
 
