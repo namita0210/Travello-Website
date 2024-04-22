6d7ca00c5de6ff360903dc030a78a63ede6720aa
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-22 10:07:11 +0530

    add offer attribute to the destination object

--- mdd/travello/views.py
+++ mdd/travello/views.py
@@ -11,18 +11,21 @@ def index(request):
     dest1.days = '7 days'
     dest1.rating = '5 star'
     dest1.img='1.png'
+    dest1.offer=False
 
     dest2 = Destination()
     dest2.name = 'Hyderabad'
     dest2.description = 'Biryani First'
     dest2.days = '4 days'
     dest2.rating = '3 star'
+    dest2.offer=True
 
     dest3 = Destination()
     dest3.name = 'Bangalore'
     dest3.description = 'Silicon Valley of India'
     dest3.days = '4 days'
     dest3.rating = '7 star'
+    dest3.offer=False
 
     dests = [dest1,dest2,dest3]
 
