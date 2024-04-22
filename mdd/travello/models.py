172daebe29d21b9ba72ae5c01f1712e59095e1df
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-22 11:35:11 +0530

    updated destination class by making it inherit from Models class and made changes to help ORM interpret it as a table

--- mdd/travello/models.py
+++ mdd/travello/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id : int
-    name : str
-    description : str
-    img : str
-    days : str
-    rating : str
-    offer :bool
+class Destination(models.Model):
+  
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    img =models.ImageField(upload_to='pics')
+    days = models.CharField(max_length=25)
+    rating = models.TextField()
+    offer = models.BooleanField(default=False)
