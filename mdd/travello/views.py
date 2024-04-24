2d46123f068fe8b13a646736814aa0a798b6f336
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-24 22:57:51 +0530

    remove statically typed destination cards and add dynamic destinations

--- mdd/travello/views.py
+++ mdd/travello/views.py
@@ -5,29 +5,9 @@ from .models import *
 # Create your views here.
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = 'Mumbai'
-    dest1.description = 'The city that never sleeps'
-    dest1.days = '7 days'
-    dest1.rating = '5 star'
-    dest1.img='1.png'
-    dest1.offer=False
+    
 
-    dest2 = Destination()
-    dest2.name = 'Hyderabad'
-    dest2.description = 'Biryani First'
-    dest2.days = '4 days'
-    dest2.rating = '3 star'
-    dest2.offer=True
-
-    dest3 = Destination()
-    dest3.name = 'Bangalore'
-    dest3.description = 'Silicon Valley of India'
-    dest3.days = '4 days'
-    dest3.rating = '7 star'
-    dest3.offer=False
-
-    dests = [dest1,dest2,dest3]
+    dests = Destination.objects.all()
 
     return render(request, 'index.html' , {'dests' : dests})
 
