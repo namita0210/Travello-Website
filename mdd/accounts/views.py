7f848c014d613517bd39b43896aa5c78f0dc2971
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-05-01 12:19:05 +0530

    add password validation in new user registration

--- mdd/accounts/views.py
+++ mdd/accounts/views.py
@@ -12,9 +12,15 @@ def register(request):
         password2 = request.POST['password2']
         email = request.POST['email']
 
-        user = User.objects.create_user(username = username , password = password1, email = email, first_name=first_name, last_name=last_name )
-        user.save()
-        print("User Created")
-        return redirect("/")
+        if password1 == password2:
+
+            user = User.objects.create_user(username = username , password = password1, email = email, first_name=first_name, last_name=last_name )
+            user.save()
+            print("User Created")
+
+        else:
+            print('Password Not Matching')  
+        return redirect('/')      
+            
     else:
         return render(request, 'register.html')
